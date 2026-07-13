23c68a3a02146b2be3943ab973b6a551a8629781
harsh1panwar <harshpanw29@gmail.com>
2026-07-13 14:55:32 +0530

    Fix: N+1 query resolved with @EntityGraph on EventRepository

--- src/main/java/com/harsh1panwar/clubmanager/repository/EventRepository.java
+++ src/main/java/com/harsh1panwar/clubmanager/repository/EventRepository.java
@@ -1,10 +1,15 @@
 package com.harsh1panwar.clubmanager.repository;
 
 import com.harsh1panwar.clubmanager.entity.Event;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
+
+    @EntityGraph(attributePaths = {"club", "organizer"})
+    List<Event> findAll();
+
     List<Event> findByClubId(Long clubId);
     List<Event> findByOrganizerId(Long organizerId);
 }
